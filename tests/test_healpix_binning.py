4fb295a60252354a58ad2c4034a0d8b3495601ae
Eli Rykoff <erykoff@stanford.edu>
2022-01-10 14:19:18 -0800

    Test healpix binning functions.

---
+++ tests/test_healpix_binning.py
@@ -0,0 +1,51 @@
+import os
+
+import numpy as np
+import healpy as hp
+
+import matplotlib
+matplotlib.use("Agg")
+
+from matplotlib.testing.compare import compare_images, ImageComparisonFailure  # noqa: E402
+import matplotlib.pyplot as plt  # noqa: E402
+
+import skyproj  # noqa: E402
+
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
+def test_healpix_binning(tmp_path):
+    """Test healpix binning functionality."""
+    np.random.seed(1234)
+
+    ra = np.random.uniform(low=30.0, high=40.0, size=10000)
+    dec = np.random.uniform(low=45.0, high=55.0, size=10000)
+    C = np.random.uniform(low=0.0, high=10.0, size=10000)
+
+    fig = plt.figure(1, figsize=(8, 5))
+    fig.clf()
+    ax = fig.add_subplot(111)
+    m = skyproj.McBrydeSkyproj(ax=ax)
+    hpxmap, im, lon_raster, lat_raster, values_raster = m.draw_hpxbin(ra, dec)
+
+    # Spot-check a pixel
+    pix = hp.ang2pix(hp.npix2nside(hpxmap.size), ra, dec, lonlat=True)
+    test, = np.where(pix == 87864)
+    assert(hpxmap[87864] == test.size)
+
+    fname = 'hpxbin.png'
+    fig.savefig(tmp_path / fname)
+    err = compare_images(os.path.join(ROOT, 'data', fname), tmp_path / fname, 40.0)
+    if err:
+        raise ImageComparisonFailure(err)
+
+    # Redo with averaging over values
+    fig = plt.figure(1, figsize=(8, 5))
+    fig.clf()
+    ax = fig.add_subplot(111)
+    m = skyproj.McBrydeSkyproj(ax=ax)
+    hpxmap, im, lon_raster, lat_raster, values_raster = m.draw_hpxbin(ra, dec, C=C)
+
+    # Spot-check the pixel
+    np.testing.assert_approx_equal(hpxmap[87864], np.mean(C[test]))
