e4e92e351f880b5441dd91deb7651b0c78c31282
Eli Rykoff <erykoff@stanford.edu>
2022-04-26 15:25:59 -0700

    Add circle and update short-hand for skyproj.

--- tests/test_lines_polygons.py
+++ tests/test_lines_polygons.py
@@ -24,24 +24,29 @@ def test_lines_polygons_mcbryde(tmp_path, lon_0):
     fig = plt.figure(1, figsize=(8, 5))
     fig.clf()
     ax = fig.add_subplot(111)
-    m = skyproj.McBrydeSkyproj(ax=ax, lon_0=lon_0)
+    sp = skyproj.McBrydeSkyproj(ax=ax, lon_0=lon_0)
 
     # Draw two geodesics, one of which will wrap around.
-    m.plot([-10., 45.], [-10., 45.], 'r-', label='One')
-    m.plot([170., 210.], [-10., 45.], 'b--', label='Two')
+    sp.plot([-10., 45.], [-10., 45.], 'r-', label='One')
+    sp.plot([170., 210.], [-10., 45.], 'b--', label='Two')
 
     # Draw two unfilled polygons, one of which will wrap around.
-    m.draw_polygon([-20, 20, 20, -20], [20, 20, 40, 40],
-                   edgecolor='magenta', label='Three')
-    m.draw_polygon([160, 200, 200, 160], [20, 20, 40, 40],
-                   edgecolor='black', label='Four')
+    sp.draw_polygon([-20, 20, 20, -20], [20, 20, 40, 40],
+                    edgecolor='magenta', label='Three')
+    sp.draw_polygon([160, 200, 200, 160], [20, 20, 40, 40],
+                    edgecolor='black', label='Four')
 
     # Draw two filled polygons, one of which will wrap around.
-    m.draw_polygon([-20, 20, 20, -20], [-20, -20, -40, -40],
-                   edgecolor='black', facecolor='red', linestyle='--', label='Five')
-    m.draw_polygon([160, 200, 200, 160], [-20, -20, -40, -40],
-                   edgecolor='red', facecolor='black', linestyle='-', label='Six')
-    m.legend()
+    sp.draw_polygon([-20, 20, 20, -20], [-20, -20, -40, -40],
+                    edgecolor='black', facecolor='red', linestyle='--', label='Five')
+    sp.draw_polygon([160, 200, 200, 160], [-20, -20, -40, -40],
+                    edgecolor='red', facecolor='black', linestyle='-', label='Six')
+
+    # Draw two circles, one empty, one filled.
+    sp.circle(40.0, -40.0, 5.0, color='blue', label='Seven')
+    sp.circle(-40.0, -40.0, 5.0, color='orange', label='Eight', fill=True)
+
+    sp.legend()
     fname = f'lines_and_polygons_{lon_0}.png'
     fig.savefig(tmp_path / fname)
     err = compare_images(os.path.join(ROOT, 'data', fname), tmp_path / fname, 10.0)
@@ -63,26 +68,26 @@ def test_lines_polygons_obmoll(tmp_path, lonlatplonp):
     fig = plt.figure(1, figsize=(8, 5))
     fig.clf()
     ax = fig.add_subplot(111)
-    m = skyproj.ObliqueMollweideSkyproj(ax=ax, lon_0=lon_0, lat_p=lat_p, lon_p=lon_p)
+    sp = skyproj.ObliqueMollweideSkyproj(ax=ax, lon_0=lon_0, lat_p=lat_p, lon_p=lon_p)
 
     # Draw two geodesics, one of which will wrap around.
-    m.plot([-10., 45.], [-10., 45.], 'r-', label='One')
-    m.plot([170., 210.], [-10., 45.], 'b--', label='Two')
+    sp.plot([-10., 45.], [-10., 45.], 'r-', label='One')
+    sp.plot([170., 210.], [-10., 45.], 'b--', label='Two')
 
     # Draw two unfilled polygons, one of which will wrap around.
-    m.draw_polygon([-20, 20, 20, -20], [20, 20, 40, 40],
-                   edgecolor='magenta', label='Three')
-    m.draw_polygon([160, 200, 200, 160], [20, 20, 40, 40],
-                   edgecolor='black', label='Four')
+    sp.draw_polygon([-20, 20, 20, -20], [20, 20, 40, 40],
+                    edgecolor='magenta', label='Three')
+    sp.draw_polygon([160, 200, 200, 160], [20, 20, 40, 40],
+                    edgecolor='black', label='Four')
 
     # Draw two filled polygons, one of which will wrap around.
     # Note that wrapped filled polygons do not currently work with oblique transforms.
-    m.draw_polygon([-20, 20, 20, -20], [-20, -20, -40, -40],
-                   edgecolor='black', facecolor='red', linestyle='--', label='Five')
-    m.draw_polygon([160, 200, 200, 160], [-20, -20, -40, -40],
-                   edgecolor='red', facecolor='black', linestyle='-', label='Six')
+    sp.draw_polygon([-20, 20, 20, -20], [-20, -20, -40, -40],
+                    edgecolor='black', facecolor='red', linestyle='--', label='Five')
+    sp.draw_polygon([160, 200, 200, 160], [-20, -20, -40, -40],
+                    edgecolor='red', facecolor='black', linestyle='-', label='Six')
 
-    m.legend()
+    sp.legend()
     fname = f'lines_and_polygons_obmoll_{lon_0}_{lat_p}_{lon_p}.png'
     fig.savefig(tmp_path / fname)
     err = compare_images(os.path.join(ROOT, 'data', fname), tmp_path / fname, 10.0)
