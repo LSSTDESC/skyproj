ed5e94383a14849749c3d697c63dc24d87a7a826
Eli Rykoff <erykoff@stanford.edu>
2026-04-21 12:42:49 -0700

    Remove proj library from setup.py.

--- setup.py
+++ setup.py
@@ -9,37 +9,9 @@ from pathlib import Path
 include_dirs = [numpy.get_include()]
 library_dirs = []
 
-proj_dir = os.environ.get("PROJ_DIR")
-if proj_dir is None:
-    proj = shutil.which("proj", path=sys.prefix)
-    if proj is None:
-        proj = shutil.which("proj")
-    if proj is None:
-        raise RuntimeError("proj not found via PROJ_DIR env var or in path.")
-
-    proj_dir = Path(proj).parent.parent
-else:
-    proj_dir = Path(proj_dir)
-
-if not proj_dir.exists():
-    raise RuntimeError(f"Could not find proj directory {proj_dir}")
-
-proj_include_dir = str(proj_dir / "include")
-# Add more logic here.
-proj_library_dir = str(proj_dir / "lib")
-
-include_dirs.append(proj_include_dir)
-library_dirs.append(proj_library_dir)
-
 extra_link_args = []
-if sys.platform == "darwin":
-    extra_link_args = [
-        f"-Wl,-rpath,{proj_library_dir}",
-        f"-L{proj_library_dir}",
-    ]
 
-
-libraries = ["proj"]
+libraries = []
 if sys.platform == 'win32':
     # Windows: no pthread or math library needed, use native
     pass
@@ -47,7 +19,6 @@ else:
     # Linux/macOS: link with pthread
     libraries.append("m")
     libraries.append("pthread")
-
 
 ext = Extension(
     "skyproj._cskyproj",
