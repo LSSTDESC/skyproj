87ec9164374b389e9829928781f14f5b416639c3
Eli Rykoff <erykoff@stanford.edu>
2026-08-20 10:26:27 -0700

    Update patch tests to add collections.

--- tests/test_patches.py
+++ tests/test_patches.py
@@ -41,3 +41,34 @@ def test_patches_mcbryde(tmp_path, lon_0):
     err = compare_images(os.path.join(ROOT, 'data', fname), tmp_path / fname, 15.0)
     if err:
         raise ImageComparisonFailure(err)
+
+
+@pytest.mark.parametrize("lon_0", [0.0, 180.0])
+def test_patch_collection_mcbryde(tmp_path, lon_0):
+    """Test drawing patches via collections."""
+    plt.rcParams.update(plt.rcParamsDefault)
+
+    fig = plt.figure(1, figsize=(8, 5))
+    fig.clf()
+    ax = fig.add_subplot(111)
+    sp = skyproj.McBrydeSkyproj(ax=ax, lon_0=lon_0)
+
+    # Draw two rectangles (geodesic), one of which will wrap around.
+    patch1 = matplotlib.patches.Rectangle([-10, 40], 20, 20, color="blue")
+    patch2 = matplotlib.patches.Rectangle([170, 40], 20, 20, color="red")
+
+    coll = matplotlib.collections.PatchCollection([patch1, patch2], match_original=True)
+    sp.ax.add_collection(coll)
+
+    # Draw two rectangles (non-geodesic), one of which will wrap around.
+    patch3 = matplotlib.patches.Rectangle([-10, -40], 20, 20, color="blue")
+    patch4 = matplotlib.patches.Rectangle([170, -40], 20, 20, color="red")
+    coll = matplotlib.collections.PatchCollection([patch3, patch4], match_original=True)
+    sp.ax.add_collection(coll, geodesic=False)
+
+    fname = f'patches_{lon_0}.png'
+    fig.savefig(tmp_path / fname)
+    plt.close(fig)
+    err = compare_images(os.path.join(ROOT, 'data', fname), tmp_path / fname, 15.0)
+    if err:
+        raise ImageComparisonFailure(err)
